341c097adb34b2aabb51ae6bb686129d794d3c59
Denys Koval <metalheadden@gmail.com>
2026-02-04 00:09:19 +0200

    pets item

--- src/screen/Home/components/PetsList.tsx
+++ src/screen/Home/components/PetsList.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import {FlatList, Text, View} from 'react-native';
+import {
+  Dimensions,
+  FlatList,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {Animal} from '../index';
+import {fonts} from '../../../constants/fonts';
+import FavoriteIcon from '../../../assets/icons/FavoriteIcon';
 
 interface PetsListProps {
   pets: Animal[];
@@ -10,13 +20,51 @@ export default function PetsList({pets}: PetsListProps) {
   return (
     <FlatList
       data={pets}
+      numColumns={2}
       renderItem={({item}) => {
         return (
-          <View>
-            <Text>{item.name}</Text>
-          </View>
+          <TouchableOpacity style={styles.item}>
+            <ImageBackground
+              source={{uri: item.images[0]}}
+              imageStyle={{borderRadius: 20}}
+              style={styles.image}
+              resizeMode={'cover'}>
+              <TouchableOpacity style={{alignItems: 'flex-end', margin: 10}}>
+                <FavoriteIcon />
+              </TouchableOpacity>
+              <View style={styles.textContainer}>
+                <Text style={styles.text}>{item.type}</Text>
+                <Text style={styles.text}>{item.age} years</Text>
+              </View>
+            </ImageBackground>
+          </TouchableOpacity>
         );
       }}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  flex: {flex: 1},
+  mainContainer: {
+    width: '100%',
+    marginHorizontal: 10,
+  },
+  item: {
+    height: 200,
+    width: Dimensions.get('window').width / 2 - 30,
+    margin: 10,
+    borderRadius: 20,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 20,
+    justifyContent: 'space-between',
+  },
+  textContainer: {margin: 15},
+  text: {
+    color: 'white',
+    fontFamily: fonts.MontserratSemiBold,
+  },
+});
